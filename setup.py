81460672a6d67c2e3864ca284bec9083aa9cd03f
Mareck1996 <danielemarelli96@gmail.com>
2014-04-30 18:19:54 +0200

    Setup

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
+    name='EmberMetin2 ItemShop',
     version='1.0',
     description='OpenShift App',
-    author='Your Name',
-    author_email='example@example.com',
+    author='Mareck',
+    author_email='danielemarelli96@gmail.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=['Django<=1.4'],
 )
